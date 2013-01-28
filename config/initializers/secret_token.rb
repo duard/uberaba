c69cf1583308dae42b33924b78898f14608e6384
Carlos Eduardo <duardbr@gmail.com>
2013-01-28 15:57:38 -0200

    primeira paulada

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HeroApp::Application.config.secret_token = '6bc68869e0cd0fb2630e71219eee9d65b6ac8727b333c2657c085ae182c4138aa0f342502e9217b8939cea5e92951988d6e02e88ee1aaafec88fdfab7be90bd9'
