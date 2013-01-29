3aa086ba5f49083b0378462e7c1a4c2652fc0a92
Carlos Eduardo <duardbr@gmail.com>
2013-01-29 10:24:53 -0200

    LOLOLOLOL - mySQL2

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 HeroApp::Application.routes.draw do
 
+  devise_for :users
+
   root :to => 'inicio#index'
 
   namespace :admin  do
     root to: "application#index"
   end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130114150713) do
-
-  create_table "marcas", :force => true do |t|
-    t.string   "descricao"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
+ActiveRecord::Schema.define(:version => 20130129122331) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
